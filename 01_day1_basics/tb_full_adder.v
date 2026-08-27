d435ddedab65c805a74ea65f54d73a0b22e08372
sooranjr5903 <sooranjr5903@gmail.com>
2026-08-27 16:29:20 +0530

    Create tb_full_adder.v

---
+++ 01_day1_basics/tb_full_adder.v
@@ -0,0 +1,36 @@
+`timescale 1ns/1ps
+
+module tb_full_adder.v;
+
+    reg a, b, cin;
+    wire sum, cout;
+
+    // Instantiate the design
+    full_adder uut (
+        .a(a),
+        .b(b),
+      .cin(cin),
+      .sum(sum),
+      .cout(cout)
+    );
+
+    initial begin
+        $dumpfile("full_adder.vcd");
+        $dumpvars(0, tb_full_adder);
+
+        // Test all combinations
+        a = 0; b = 0; cin = 0; #10;
+        a = 0; b = 1; cin = 0; #10;
+        a = 1; b = 0; cin = 0; #10;
+        a = 1; b = 1; cin = 0; #10;
+
+        a = 0; b = 0; cin = 1; #10;
+        a = 0; b = 1; cin = 1; #10;
+        a = 1; b = 0; cin = 1; #10;
+        a = 1; b = 1; cin = 1; #10;
+
+        $display("Simulation finished");
+        $finish;
+    end
+
+endmodule
