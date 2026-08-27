45da5ce3d2e3ce4b8c3ea6ae89dbed396bde7497
sooranjr5903 <sooranjr5903@gmail.com>
2026-08-27 16:36:06 +0530

    Update tb_full_adder.v

--- 01_day1_basics/tb_full_adder.v
+++ 01_day1_basics/tb_full_adder.v
@@ -1,6 +1,6 @@
 `timescale 1ns/1ps
 
-module tb_full_adder.v;
+module tb_full_adder;
 
     reg a, b, cin;
     wire sum, cout;
@@ -23,7 +23,6 @@ module tb_full_adder.v;
         a = 0; b = 1; cin = 0; #10;
         a = 1; b = 0; cin = 0; #10;
         a = 1; b = 1; cin = 0; #10;
-
         a = 0; b = 0; cin = 1; #10;
         a = 0; b = 1; cin = 1; #10;
         a = 1; b = 0; cin = 1; #10;
