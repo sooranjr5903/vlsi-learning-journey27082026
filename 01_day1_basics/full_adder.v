fd944a843e371aad41f31512b7e70bdff108cab8
sooranjr5903 <sooranjr5903@gmail.com>
2026-08-27 16:32:22 +0530

    Update full_adder.v

--- 01_day1_basics/full_adder.v
+++ 01_day1_basics/full_adder.v
@@ -2,6 +2,6 @@ module full_adder (
   input wire a, b, cin,
   output wire sum, cout
 );
-assign sum=a^b^cin
-       cout=(a&b)|(b&cin)|(a&cin);
+assign sum=a^b^cin;
+assign cout=(a&b)|(b&cin)|(a&cin);
 endmodule
